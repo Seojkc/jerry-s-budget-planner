ed3e421e4a438c1b0fd826181f8f9ba480f59f68
Seojkc <seojameskc007@gmail.com>
2024-10-19 09:17:17 -0400

    add:  added the navbar

--- budget-planner/src/App.js
+++ budget-planner/src/App.js
@@ -2,23 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import MyNavbar from './MyNavBar';
 import Dashboard from './Pages/Dashboard';
+import Income from './Pages/Income';
 import React from 'react';
 import { Container,Row, Col } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Container fluid>
-        <Row>
-          <Col md={3}>
-            <MyNavbar/>
-          </Col>
-          <Col md={9}>
-            <Dashboard/>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <Router>
+      <div className="App">
+        <Container fluid>
+          <div className='Row-Container'>
+            <div className='nav-bar'>
+              <MyNavbar/>
+            </div>
+            <div className='main-page'>
+              <Routes>
+                <Route path="/" element={<Navigate to="/dashboard" />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/income" element={<Income />} />
+
+              </Routes>
+            </div>
+            
+          </div>
+        </Container>
+      </div>
+    </Router>
+    
   );
 }
 
--- budget-planner/src/MyNavBar.jsx
+++ budget-planner/src/MyNavBar.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Navbar, Nav, Flex } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './StyleSheets/MyNavBar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import FontAwesome component
-import { faHome, faDollarSign, faCog } from '@fortawesome/free-solid-svg-icons'; // Import specific icons
+import { faHome, faDollarSign, faCog, faFileChartColumn } from '@fortawesome/free-solid-svg-icons'; // Import specific icons
 
 
 const MyNavbar = () => {
@@ -11,9 +12,22 @@ const MyNavbar = () => {
     <Navbar bg="dark" variant="dark" className="flex-column container">
       <Nav className="flexRow">
         <div className="flexRowChild">
-          <Nav.Link  href="#Dashboard"><FontAwesomeIcon icon={faHome}  style={{ fontSize: '22px', marginRight:'10px' ,marginBottom:'2px' }}/> Dashboard</Nav.Link>
-          <Nav.Link  href="#Income"><FontAwesomeIcon icon={faDollarSign}  style={{ fontSize: '22px', marginRight:'10px' ,marginBottom:'2px' }}/> Income</Nav.Link>
-
+          <Link to="/dashboard" className="nav-link">
+            <FontAwesomeIcon icon={faHome} style={{ fontSize: '22px', marginRight: '10px', marginBottom: '2px' }} /> 
+            Dashboard
+          </Link>          
+          <Link to="/income" className="nav-link">
+            <FontAwesomeIcon icon={faDollarSign} style={{ fontSize: '22px', marginRight: '10px', marginBottom: '2px' }} /> 
+            Income
+          </Link>
+          <Link to="/Report" className="nav-link">
+            <FontAwesomeIcon icon={faDollarSign} style={{ fontSize: '22px', marginRight: '10px', marginBottom: '2px' }} /> 
+            Report
+          </Link>
+          <Link to="/income" className="nav-link">
+            <FontAwesomeIcon icon={faCog} style={{ fontSize: '22px', marginRight: '10px', marginBottom: '2px' }} /> 
+            Bills
+          </Link>
         </div>
         
       </Nav>
