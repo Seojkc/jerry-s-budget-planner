9c35310f6c8bf9cbc91eacfdfc55fcc83a1747cc
Seojkc <seojameskc007@gmail.com>
2024-10-19 09:16:36 -0400

    connect :  connected the react app with backend server

--- budget-planner/src/Pages/Dashboard.jsx
+++ budget-planner/src/Pages/Dashboard.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import '../StyleSheets/DashBoard.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 
 function Dashboard() {
+
+    const [expenses, setExpenses] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/expenses')
+        .then(response => {
+            setExpenses(response.data);
+        })
+        .catch(error => {
+            console.error("There was an error fetching the expenses!", error);
+        });
+    }, []);
+
     return (
-        <div className="bg-dark text-white p-5">
+        <div className="Container">
             <h1>Hello World</h1>
-            <p>This is the main content area.</p>
+            <p>This is the main cont ent area.</p>
+            <ul>
+                {expenses.map(expense => (
+                    <li key={expense.expenseID}>{expense.description} - ${expense.amount}</li>
+                ))}
+            </ul>
         </div>
     );
 }
